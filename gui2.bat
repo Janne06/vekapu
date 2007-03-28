eb81ddb2f8688ab8ad765e711837811dba2e8479
janne.ilonen <janne.ilonen@40a52378-9e29-0410-9dd8-ad8342e501ca>
2007-03-28 18:50:20 +0000

    Lisätään 'BrowserLauncher2-10.jar' mukaan.

--- gui2.bat
+++ gui2.bat
@@ -1,3 +1,3 @@
-@rem $Id: guine $
+@rem $Id$
 
-java -cp ./lib/vekapu.jar;./lib/mail.jar;./lib/activation.jar;./lib/log4j.jar;./lib/jmdi.jar;. net.vekapu.gui3.VekapuMdiGui %1 %2 %3 %4 %5
+java -cp ./lib/vekapu.jar;./lib/mail.jar;./lib/activation.jar;./lib/log4j.jar;./lib/jmdi.jar;./lib/BrowserLauncher2-10.jar;. net.vekapu.gui3.VekapuMdiGui %1 %2 %3 %4 %5
--- gui2.sh
+++ gui2.sh
@@ -8,6 +8,6 @@
 
 # Full path to java if using kcron
 #/where/is/java
-java -cp ./lib/vekapu.jar:./lib/mail.jar:./lib/activation.jar:./lib/log4j.jar:./lib/jmdi.jar:.  net.vekapu.gui3.VekapuMdiGui $1 $2 $3 $4 $5
+java -cp ./lib/vekapu.jar:./lib/mail.jar:./lib/activation.jar:./lib/log4j.jar:./lib/jmdi.jar:./lib/BrowserLauncher2-10.jar:.  net.vekapu.gui3.VekapuMdiGui $1 $2 $3 $4 $5
 
 
