f933e49f26c7028128d9e28f7a463783577eb9d7
janne.ilonen <janne.ilonen@40a52378-9e29-0410-9dd8-ad8342e501ca>
2009-07-06 17:25:24 +0000

    Startti file vain MDI gui:lle.

--- trunk/gui.sh
+++ trunk/gui.sh
@@ -4,4 +4,10 @@
 # Make this file executable:
 # chmod 755 vekapu.sh
 
-java -cp ./lib/vekapu.jar:./lib/mail.jar:./lib/activation.jar:./lib/log4j.jar:. net.vekapu.gui.VekapuGuiStarter $1 $2 $3 $4 $5
+#cd /to/you/vekapu/vekapu-n.n.n/
+
+# Full path to java if using kcron
+#/where/is/java
+java -cp ./lib/vekapu.jar:./lib/mail.jar:./lib/activation.jar:./lib/log4j.jar:./lib/jmdi.jar:./lib/BrowserLauncher2-10.jar:.  net.vekapu.gui3.VekapuMdiGui $1 $2 $3 $4 $5
+
+
