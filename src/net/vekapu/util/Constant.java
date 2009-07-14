737269a2f270652e190ee9e062def1bcfa590768
janne.ilonen <janne.ilonen@40a52378-9e29-0410-9dd8-ad8342e501ca>
2009-07-14 19:41:41 +0000

    Kehitys version numeroksi 2.5.1

--- src/net/vekapu/util/Constant.java
+++ src/net/vekapu/util/Constant.java
@@ -1,4 +1,4 @@
-//////////////////////////////////////////////////////////////////////////////
+﻿//////////////////////////////////////////////////////////////////////////////
 //
 // Filename: Constant.java
 //
@@ -37,9 +37,9 @@ public class Constant {
 
 	/*
 	 Päivitä molemmat (versio & VERSION) numerot AINA SAMOIKSI. 
-	 versio = 2.5
+	 versio = 2.5.1
 	 */
-	private final static String VERSION = "2.5";
+	private final static String VERSION = "2.5.1";
 
 	private final static String URL = "http://www.vekapu.net/";
 
